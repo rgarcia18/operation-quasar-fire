47a3e4d78357d220f4803691baffab2a5ec2a281
rgarcia18 <ruga-18@hotmail.com>
2022-05-05 00:25:45 -0500

    first commit - project structure

---
+++ src/main/java/com/quasarfire/quasar/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.quasarfire.quasar;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(QuasarApplication.class);
+	}
+
+}
